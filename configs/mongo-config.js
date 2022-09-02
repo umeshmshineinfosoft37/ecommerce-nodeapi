269b88f6c68d6f85984e319a9f682b4b0e05227a
DESKTOP-ND3P6DD\Shine Infosoft <shineinfosoft24@gmail.com>
2022-09-02 12:51:31 +0530

    app cli

--- configs/mongo-config.js
+++ configs/mongo-config.js
@@ -1,6 +1,6 @@
 
 // export mongodb url
-const mongoConfig= 'mongodb://localhost:27017/ecommerce-node'
-//  const mongoConfig= 'mongodb+srv://umeshm:Umesh@123@cluster0.trkuouf.mongodb.net/ecommerce-node?retryWrites=true&w=majority'
+// const mongoConfig= 'mongodb://localhost:27017/ecommerce-node'
+ const mongoConfig= 'mongodb+srv://umeshm:Umesh@123@cluster0.trkuouf.mongodb.net/ecommerce-node?retryWrites=true&w=majority'
 module.exports = mongoConfig;
 
