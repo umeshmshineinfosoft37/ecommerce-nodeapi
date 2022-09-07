65ec51aa37ba38b197e1b2d3965352716b7990cd
DESKTOP-ND3P6DD\Shine Infosoft <shineinfosoft24@gmail.com>
2022-09-07 19:11:33 +0530

    add strip

--- configs/paypal-config.js
+++ configs/paypal-config.js
@@ -1,1 +1,8 @@
 // export paypal sandbox configs like 'mode', 'client_id', 'client_secret'
+
+const paypal_config ={
+    mode: 'sandbox', //sandbox or live
+    client_id: 'AV48yI4zF4p5jajFb1CHweo-Zn49oafV_wiEsAZ4w_eWDxhbU9gzfEMMhNEOz8O6ZLwsJQItDpFhQSIn',
+    client_secret: 'ENaFj_Gn10ch6OBRNKAVqkmLboBIhSZ5uM-ARDUN58m68XfKyQ-MntWaUtuqivSs9FHdOwLSpLgpNXJs'
+}
+module.export = {paypal_config};
--- helper/profileUpload.js
+++ helper/profileUpload.js
@@ -1,15 +1,6 @@
-const multer = require('multer')
-const path = require('path')
-    // SET Storage
-    // let storage = multer.diskStorage({
-    //     destination: function(req, file, cb) {
-    //         cb(null, "public");
-    //     },
-    //     filename: function(req, file, cb) {
-    //         const fileName = file.originalname.replace(" ", "-");
-    //         cb(null, fileName + "-" + Date.now());
-    //     },
-    // });
+const multer = require('multer');
+const path = require('path');
+
 const multerStorage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, "public");
@@ -28,19 +19,6 @@ const multerFilter = (req, file, cb) => {
         cb(new Error("Not a jpg/jpeg/png File!!"), false);
     }
 };
-// const profileUpload = multer({
-//         storage,
-//         // limits: {
-//         //     fileSize: 1000000 // 1000000 Bytes = 1 MB
-//         // },
-//         fileFilter(req, file, cb) {
-//             if (!file.originalname.match(/\.(png|jpg)$/)) {
-//                 // upload only png and jpg format
-//                 return cb(new Error('Please upload a Image'))
-//             }
-//             cb(undefined, true)
-//         }
-//     })
 const profileUpload = multer({ storage: multerStorage, fileFilter: multerFilter });
 
 module.exports = { profileUpload };
